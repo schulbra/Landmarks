6822cc1e0645a15e7a0b5bb01b872c5da3a32ec1
Brandon Schultz <45749320+schulbra@users.noreply.github.com>
2022-11-21 08:03:31 +0000

    Add files via upload
    switching from ciera to brandon ios login, copying over work for access on new login

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
